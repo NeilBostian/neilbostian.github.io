95f89a60d84f23a4ac31f47730233526fd256199
Neil Bostian <neil.bostian@gmail.com>
2021-08-01 22:16:37 -0400

    feat: add doomphone content

--- src/pages/doomphone.js
+++ src/pages/doomphone.js
@@ -89,7 +89,7 @@ export default function DoomPhone() {
             <a href="https://en.wikipedia.org/wiki/Doom_(1993_video_game)">DOOM</a>
           </>
         }
-        publishDate="2021-07-15"
+        publishDate="2021-08-01"
       />
 
       <h3>Contents</h3>
@@ -109,10 +109,14 @@ export default function DoomPhone() {
             <li>
               <LinkScrollToElement elementId="doomphone-keypad-driver">Keypad Driver</LinkScrollToElement>
             </li>
-            <li>Putting Everything Together</li>
+            <li>
+              <LinkScrollToElement elementId="doomphone-compiling-doom">Compiling DOOM</LinkScrollToElement>
+            </li>
           </ul>
         </li>
-        <li>Conclusion</li>
+        <li>
+          <LinkScrollToElement elementId="doomphone-conclusion">Conclusion</LinkScrollToElement>
+        </li>
       </ol>
 
       <CenterImg src={doomTitle} width="600px" alt="DOOM logo" />
@@ -131,7 +135,7 @@ export default function DoomPhone() {
       </p>
       <p>
         The phone has a basic web browser intended for 3rd party IT teams to provision custom apps to their users. I
-        created a basic html page with <Code>&lt;input type='file' /&gt;</Code> to browse the file system.
+        created a html page with <Code>&lt;input type='file' /&gt;</Code> to browse the file system.
       </p>
       <CenterImg src={polycomFileBrowser} alt="Polycom phone file browser" width="480px" />
       <p>
@@ -141,23 +145,21 @@ export default function DoomPhone() {
         parent directory.
       </p>
       <p>
-        Ultimately the ability to move files didn't get me closer to hacking the phone and I needed a new strategy.
-        Polycom has a <a href="http://voipt2.polycom.com/">large list of firmware versions</a> and allows you to freely
+        Being able to move files was great if you wanted to break the phone, but ultimately didn't get me closer to
+        hacking it. I needed a new strategy. Polycom has a{' '}
+        <a href="http://voipt2.polycom.com/">large list of firmware versions</a> and allows you to freely
         upgrade/downgrade to any version supported by your model. <Italic>Perhaps I can exploit that...</Italic>
       </p>
       <p>
         Each night after work I tried to reverse engineer Polycom's firmware to give myself a backdoor. Every attempt to
         update my phone using custom firmware was rejected. These phones are also <Italic>painfully slow</Italic>. A
-        full update cycle runs more than 10 minutes, so I was unlikely to make progress by trying more of the same.
+        full update cycle runs more than 10 minutes, so I was unlikely to make progress through brute force.
       </p>
       <p>
         So I did the rational thing and gave up. But I gained knowledge and experience — a worthy trade. I'll admit, in
-        retrospect this was a really ambitious task. I also have a tendency to underestimate difficulty.
-      </p>
-      <p>
-        Nobody succeeds in the software industry without tenacity. I don't know what came over me in May '21, almost 4
-        years later. Maybe it was a subconscious feeling of defeat. So I did the irrational thing and bought a
-        refurbished Polycom VXX600.
+        retrospect this was a really ambitious task. I also have a tendency to underestimate difficulty. But nobody
+        succeeds in the software industry without tenacity. I don't know what came over me in June '21, almost 4 years
+        later. Maybe it was a subconscious feeling of defeat. So I bought a refurbished Polycom VXX600.
       </p>
 
       <h3>A Brief Word of Wisdom</h3>
@@ -168,8 +170,8 @@ export default function DoomPhone() {
       </p>
       <p>
         1. Nobody buys a Polycom phone because it has a good admin interface. Polycom sells phones because they provide
-        a product that looks polished, affordable, and suitable for their customer. That means Polycom spends more
-        resources (employee time) developing end-user features, and fewer resources developing tools to provision and
+        a product that looks polished, affordable, and suitable for the end user. That means Polycom spends more
+        resources (employee time) developing end user features, and fewer resources developing tools to provision and
         administrate.
       </p>
       <p>
@@ -184,7 +186,7 @@ export default function DoomPhone() {
       <p>
         With that wisdom in mind, let's see if we can find a vulnerability in the VXX600's admin interface. When factory
         reset, I can login to my phone's admin settings using password <Code>456</Code>. Poking around reveals a section
-        titled "Web Server Configuration". Let's enable that and save settings. The phone will reboot itself.
+        titled "Web Server Configuration". Let's enable that and save settings. The phone reboots itself.
       </p>
       <CenterImg
         src={polycomAdminWebserver}
@@ -292,7 +294,10 @@ done`}
           <Code>/usr/local/2</Code> — This stores the output logs from the scripts we drop in directory <Code>1</Code>
         </li>
       </ul>
-      <p>Next we upload a file using our C# script:</p>
+      <p>
+        Next we upload <Code>S70dbus-neil-bootstrap.sh</Code> using our C# script. This file is just a launcher that
+        starts <Code>neil-hack.sh</Code> in the background so the rest of the phone completes initialization.
+      </p>
       <ul>
         <li>
           <Code>fileName = "../usr/local/etc/rcS.d/S70dbus-neil-bootstrap.sh";</Code>
@@ -302,14 +307,15 @@ done`}
         </li>
       </ul>
       <p>
-        Then we upload <Code>neil-hack.sh</Code>
+        Then we upload <Code>neil-hack.sh</Code> which continuously monitors directory <Code>/usr/local/1</Code> for
+        scripts and executes them.
       </p>
       <ul>
         <li>
           <Code>fileName = "../usr/local/neil-hack.sh";</Code>
         </li>
         <li>
-          <Code>fileContent: </Code>
+          <Code>fileContent: 👇🏻</Code>
         </li>
       </ul>
       <CopyBlock
@@ -333,8 +339,8 @@ done`}
         theme={github}
       />
       <p>
-        Finally, we reboot our phone. On startup, <Code>neil-hack.sh</Code> will run in the background waiting for us to
-        drop scripts into dir <Code>/usr/local/1</Code>. Once the phone reboots, upload a sample script to confirm and
+        Finally we reboot our phone. On startup, <Code>neil-hack.sh</Code> will run in the background waiting for us to
+        drop scripts into <Code>/usr/local/1</Code>. When the reboot completes, upload a sample script to confirm and
         check the output in <Code>/usr/local/2</Code>.
       </p>
       <ul>
@@ -354,7 +360,7 @@ done`}
           <Code>fileName = "../usr/local/1/enable-ssh.sh";</Code>
         </li>
         <li>
-          <Code>fileContent: </Code>
+          <Code>fileContent: 👇🏻</Code>
         </li>
       </ul>
       <CopyBlock
@@ -374,19 +380,21 @@ done`}
       <CenterImg src={sshTerminal} width="600px" alt="Ubuntu terminal connected to the phone via ssh" />
 
       <h3 id="doomphone-making-it-run-doom">Make it run DOOM</h3>
-      <p>Before digging into the details of running doom, we need some tools:</p>
+      <p>Before digging into the details of running DOOM, we need some tools:</p>
       <ul>
         <li>
           <Code>
             <a href="https://packages.ubuntu.com/bionic/sshpass">sshpass</a>
           </Code>{' '}
-          — This lets us script our ssh commands with password iterate more quickly
+          — This lets us script our ssh commands with the root password to iterate more quickly. This is ok since our
+          root password isn't sensitive.
         </li>
         <li>
           <Code>
             <a href="https://packages.ubuntu.com/bionic/gcc-arm-linux-gnueabi">arm-linux-gnueabi-gcc</a>
           </Code>{' '}
-          — An ARMv6 linux cross compiler
+          — An ARMv6 linux cross compiler. This allows us to compile code on our host PC into a binary that is
+          compatible with the ARMv6 processor that runs our phone.
         </li>
       </ul>
       <p>
@@ -394,7 +402,7 @@ done`}
         on the phone to confirm we have a working build environment.
       </p>
 
-      <h4 id="doomphone-display-driver">Writing the Display Driver</h4>
+      <h3 id="doomphone-display-driver">Writing the Display Driver</h3>
       <p>
         <Code>ls /dev</Code> reveals <Code>/dev/fb0</Code> which we can use to read and write to the screen. This can be
         quickly tested by writing junk into the framebuffer: <Code>cat /dev/urandom &gt; /dev/fb0</Code>. To write
@@ -408,8 +416,9 @@ done`}
         theme={github}
       />
       <p>
-        Then we need to figure out the pixel encoding. Run <Code>take_screenshot screenshot.raw</Code> (from
-        shell-extensions) to capture the framebuffer contents. To determine format we can upload the image to{' '}
+        Then we need to figure out the pixel encoding. Run <Code>take_screenshot screenshot.raw</Code> (from{' '}
+        <a href="https://github.com/NeilBostian/DoomPhone/blob/main/shell-extensions.sh#L23">shell-extensions</a>) to
+        capture the framebuffer contents. To determine format we can upload the image to{' '}
         <a href="http://rawpixels.net">rawpixels.net</a> and play with the settings until it looks correct.
       </p>
       <CenterImg src={rawPixels} width="600px" alt="Our framebuffer settings decoded using rawpixels.net" />
@@ -424,7 +433,7 @@ done`}
         "As long as it works..." 😉
       </p>
       <p>
-        Now we write a simple C program that writes into the framebuffer.{' '}
+        Now we write a C program that writes into the framebuffer.{' '}
         <a href="https://github.com/NeilBostian/DoomPhone/tree/main/display-driver">Check out the source code here</a>.
         When we run this, we get a nice rainbow that scrolls across the screen.{' '}
         <Italic>
@@ -435,14 +444,14 @@ done`}
       </p>
       <CenterImg src={displayDriver} width="480px" alt="Screenshot of display driver rainbow" />
 
-      <h4 id="doomphone-keypad-driver">Writing the Keypad Driver</h4>
+      <h3 id="doomphone-keypad-driver">Writing the Keypad Driver</h3>
       <p>
         The keypad driver is relatively straight forward. The init script <Code>/etc/rcS.d/S13keypad</Code> indicates
         keypad events come through <Code>/dev/input/event0</Code>. Let's write a program to read from stdin and output
         the bytes as hex values (<a href="https://github.com/NeilBostian/DoomPhone/tree/main/keypad-driver">see here</a>
-        ). Then we can interpret the scancodes and translate it to key events.{' '}
-        <Italic>Note the build script launches the sample program with </Italic>{' '}
-        <Code> cat /dev/input/event0 | /keypad-driver</Code> so the events make it to stdin.
+        ). Then we can interpret the scancodes and translate them to key events.{' '}
+        <Italic>Note the build script launches the sample program with </Italic>
+        <Code>cat /dev/input/event0 | /keypad-driver</Code> <Italic>so the events make it to stdin</Italic>.
       </p>
       <p>
         Key events correspond to pressing or releasing a button. Each event sends a 16-byte payload, with the 11th byte
@@ -450,6 +459,122 @@ done`}
         or released.
       </p>
       <CenterImg src={scancodes} width="585px" alt="Screenshot of display driver rainbow" />
+
+      <h3 id="doomphone-compiling-doom">Compiling DOOM</h3>
+      <p>
+        I found <a href="https://github.com/ozkl/doomgeneric">this repository</a> (doomgeneric) which provided a simple
+        interface to port doom. It isolates the platform dependent parts of DOOM's source code (video / input) to a
+        single file / header. My implementation of the various doomgeneric (DG) methods is in{' '}
+        <a href="https://github.com/NeilBostian/DoomPhone/blob/main/doom/doomgeneric_plcm.c">doomgeneric_plcm.c</a>, and
+        uses a lot of code from the display/keypad drivers.
+      </p>
+      <p>
+        The makefile was easily{' '}
+        <a href="https://github.com/NeilBostian/DoomPhone/commit/7895babeaadb02c95b00f872c7e034bfc84dd20e#diff-b83c8825b764d87279c297c11e178fae7868764d65e2828e6bfb5a61ca441c96R15">
+          updated to use our cross compiler
+        </a>
+        , and I added a <a href="https://github.com/NeilBostian/DoomPhone/blob/main/doom/build.sh">build.sh</a> script
+        to run make and copy the binary to the phone. I also removed platform files for windows and other doomgeneric
+        targets.
+      </p>
+      <p>
+        DOOM requires a resource file to run. I used the shareware version, <Code>DOOM1.WAD</Code>, which is available
+        online. DOOM also has many command line arguments, you can find them by checking references to methods{' '}
+        <a href="https://github.com/NeilBostian/DoomPhone/search?q=M_CheckParm">
+          <Code>M_CheckParm</Code>
+        </a>{' '}
+        and{' '}
+        <a href="https://github.com/NeilBostian/DoomPhone/search?q=M_CheckParmWithArgs">
+          <Code>M_CheckParmWithArgs</Code>
+        </a>
+        . Polycom phone buttons are converted to DOOM keypresses in method{' '}
+        <a href="https://github.com/NeilBostian/DoomPhone/blob/main/doom/doomgeneric_plcm.c#L122">
+          <Code>convertToDoomKey</Code>
+        </a>
+        .
+      </p>
+      <p>
+        Lastly, I setup an <a href="https://github.com/NeilBostian/DoomPhone/blob/main/doom/install.sh">install</a>{' '}
+        script to configure the phone and install necessary files. Now from our host PC we can run the scripts to
+        install, build, and launch doom:
+      </p>
+      <CopyBlock
+        text={`# enter doom directory
+cd doom
+
+# Source  our shell extensions
+. ../shell-extensions.sh
+
+# Run install script to configure the phone
+./install.sh
+
+# Compile doom and launch it
+./build.sh && ssh_exec "/usr/local/doom/run_doom.sh"`}
+        language="shell"
+        showLineNumbers={false}
+        theme={github}
+      />
+
+      <h3 id="doomphone-conclusion">Conclusion</h3>
+      <p>
+        I learned a lot of valuable lessons from this experience. I pulled out nearly every skill I've trained in 10
+        years to make this phone run DOOM, and I also learned many new things about linux and embedded systems. But
+        skills and experience are useless if they're not applied properly. The most valuable application of skills is
+        through time and effort that multiplies their effects.
+      </p>
+      <p>
+        Another lesson which I knew, but was reiterated during this project, is that sometimes the best way to solve a
+        really hard problem is to take a break. I think there's two important reasons to take a break:
+      </p>
+      <p>
+        1. <a href="https://en.wikipedia.org/wiki/Recency_bias">Recency bias</a> is a real effect and clouds your
+        ability to evaluate all information equally. It's easy to overlook obvious issues and solutions when you've
+        spent so long looking at them. I think every developer at some point can relate to spending lots of time
+        debugging an issue that turned out to be a typo.
+      </p>
+      <p>
+        Another way I experience recency bias is in my writing. I always have trouble proofreading something I wrote
+        within the past few minutes. An hour away from a draft helps me spot grammatical errors, sentences that are
+        unclear or unnecessary, and the structure of the overall piece. A good night's sleep is even better.
+      </p>
+      <p>
+        2. You are constantly growing as a person, and during time away from a problem you may gain new perspectives
+        that help solve it. The experience I gained throughout my early 20s has benefited my problem solving in a
+        profound way. I am shocked at how my non-technical skills augment my ability to solve technical challenges.
+      </p>
+
+      <h4>A side note on "experience"</h4>
+      <p>
+        Across every industry, people discuss experience in terms of years. (ex. "I have 5 years of experience as a
+        professional XYZ") This is a really flawed method to evaluate <Italic>anyone</Italic>, especially people in fast
+        moving industries like tech.
+      </p>
+      <p>
+        Richard Feynman filmed a series called "Fun to Imagine" where he discusses many things. In particular, the
+        section titled <a href="https://www.youtube.com/watch?v=P1ww1IXRfTA&amp;t=3301s">Ways of Thinking</a> struck me.
+        Feynman recalls an experiment between him and mathematician John Tukey. The experiment involved counting in
+        their heads. Feynman was able to read while counting, but could not speak. Conversely, John Tukey was unable to
+        read, but could speak just fine. Feynman highlights this experiment as evidence for how differently each person
+        thinks about everything.
+      </p>
+      <p>
+        I think this idea extends to peoples' experience. If everyone's experience is different then it can't be
+        measured by <Italic>how much time</Italic> we spend experiencing something. The only meaningful measurement of
+        my skills is what I accomplish with them. Having said that we still have the problem that people use years of
+        experience as a cursory measure of someone's potential skill. I don't know how to solve the problem, but the
+        idea seems worthy of discussion.
+      </p>
+      <p>
+        You are the sum of your experiences, not in years, but instead in some imaginary "experience points". Live your
+        life such that you maximize XP points in the various skills and achievements that matter most to you. For me,
+        one of those skills happens to be making DOOM run in places it wasn't intended.
+      </p>
+      <p>Thanks for reading.</p>
+      <p>
+        <Italic>Neil</Italic>
+      </p>
+      <p></p>
+      <p>Questions, comments, or other feedback? Drop a reply on twitter or </p>
     </>
   );
 }
